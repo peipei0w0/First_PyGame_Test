70002374556fa0536aa27770eaa376594dd41b6a
陳珮瑄 <59371107+peipei0w0@users.noreply.github.com>
2022-08-13 20:13:34 +0800

    Delete OnePerson_2022.08.06 directory

--- OnePerson_2022.08.06/test.py
+++
@@ -1,7 +0,0 @@
-# for i in range(10):
-#     #name = "name_" + str(i)
-#     exec('name{} = {}'.format(i, i))
-#     print(name0)
-# for i in range(5):
-#     name{}.format(i) = i
-#     print(name{}.format(i))
