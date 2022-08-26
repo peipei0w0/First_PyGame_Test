3baaf1732297010a2655fa0db19f3d3f3c3ba528
陳珮瑄 <59371107+peipei0w0@users.noreply.github.com>
2022-08-26 14:28:08 +0800

    Add files via upload

---
+++ OnePerson/One Person_2022.08.26/time.py
@@ -0,0 +1,14 @@
+import time
+
+def countdown(num_of_secs):
+    while num_of_secs:
+        m, s = divmod(num_of_secs, 60)
+        min_sec_format = s
+        #print(min_sec_format)
+        time.sleep(1)
+        num_of_secs -= 1
+
+    print(0)
+
+inp = int(input('輸入要倒數的秒數：'))
+countdown(inp)
